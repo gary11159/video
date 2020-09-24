28bdb0bda8bddee2da677ad40451ca3be3d7f725
Gary.Yang <gary.yang@hy-tech.com.tw>
2020-09-24 14:43:07 +0800

    Updates

---
+++ precache-manifest.ba76818acee2d7414201d126ab208812.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1368a7efb0ead2ff4c319816435559d5",
+    "url": "/video/index.html"
+  },
+  {
+    "revision": "c64e4d4fd56c3cd54c4a",
+    "url": "/video/static/css/main.f0be8e48.chunk.css"
+  },
+  {
+    "revision": "0d5ad6facd3e517ca346",
+    "url": "/video/static/js/2.146bf2b5.chunk.js"
+  },
+  {
+    "revision": "4cfaa7c28b073e304683701afe193f9a",
+    "url": "/video/static/js/2.146bf2b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c64e4d4fd56c3cd54c4a",
+    "url": "/video/static/js/main.31f0bf73.chunk.js"
+  },
+  {
+    "revision": "057d9ce2f4a9455858c7",
+    "url": "/video/static/js/runtime-main.6face553.js"
+  },
+  {
+    "revision": "083b0bcecce11e96e8705cebcf41fa00",
+    "url": "/video/static/media/ad.083b0bce.jpg"
+  },
+  {
+    "revision": "7a960949a5d51cf8b6ffef964d57feec",
+    "url": "/video/static/media/loading.7a960949.gif"
+  }
+]);
