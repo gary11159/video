5bf82e8a0815c28e02005e0f89cc0f22bfd51a3c
Gary.Yang <gary.yang@hy-tech.com.tw>
2020-10-12 11:36:49 +0800

    Updates

---
+++ precache-manifest.8f2e5756008a56baedfaf68bd6c545d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ba7e7735ef00ba767c637bd2ff9a84d",
+    "url": "/video/index.html"
+  },
+  {
+    "revision": "35dc9990a06d516d7d81",
+    "url": "/video/static/css/main.54ac154a.chunk.css"
+  },
+  {
+    "revision": "944a577bf4ba56f29715",
+    "url": "/video/static/js/2.849bac28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/video/static/js/2.849bac28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35dc9990a06d516d7d81",
+    "url": "/video/static/js/main.382f1ce3.chunk.js"
+  },
+  {
+    "revision": "057d9ce2f4a9455858c7",
+    "url": "/video/static/js/runtime-main.6face553.js"
+  },
+  {
+    "revision": "7a960949a5d51cf8b6ffef964d57feec",
+    "url": "/video/static/media/loading.7a960949.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/video/precache-manifest.fe1a6728844475795c1176d6bdf238be.js"
+  "/video/precache-manifest.8f2e5756008a56baedfaf68bd6c545d6.js"
 );
 
 self.addEventListener('message', (event) => {
