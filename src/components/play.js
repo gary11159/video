a4b3f88b9832b36fdb788f785c0a9915b8933a9c
Gary.Yang <gary.yang@hy-tech.com.tw>
2020-10-12 11:38:04 +0800

    作品版

--- src/components/play.js
+++ src/components/play.js
@@ -1,13 +1,13 @@
 import React from "react";
-import ReactHlsPlayer from 'react-hls-player';
-import Advertise from '../public/ad.jpg';
+// import ReactHlsPlayer from 'react-hls-player';
+// import Advertise from '../public/ad.jpg';
 import { useLocation } from "react-router";
-// import ReactPlayer from 'react-player';
+import ReactPlayer from 'react-player';
 
 function Play(props) {
     const videoItem = useLocation().state;
-    const videoEl = React.useRef(null);
-    // const [videoHeight, setVideoHeight] = React.useState(window.screen.width / 2.4);
+    // const videoEl = React.useRef(null);
+    const [videoHeight, setVideoHeight] = React.useState(window.screen.width / 2.4);
     React.useEffect(() => {
         if (props.curTab !== 'play') {
             props.setCurTab('play');
@@ -21,9 +21,6 @@ function Play(props) {
         })
     });
 
-    // function videoHeightHandler() {
-    //     setVideoHeight(window.screen.width / 2.4);
-    // }
     function adHandler(action) {
         if (videoItem === undefined || videoItem === null) return;
         if (action === '' || action === 'none') {
@@ -36,14 +33,14 @@ function Play(props) {
         document.getElementById('advertisePic').style.transform = 'translate(0, ' + changeHeight + 'px)';
     }
 
-    // let urlVideo = videoItem !== undefined ? "https://www.youtube.com/watch?v=" + videoItem.item.id : null;
+    let urlVideo = videoItem !== undefined ? "https://www.youtube.com/watch?v=" + videoItem.item.id : null;
     return (
         <>
             { videoItem !== undefined ?
                 <>
-                    {/* <div className="video" id="videoBox" style={{ marginTop: '50px' }}> */}
-                    <div className="video" id="videoBox">
-                        <ReactHlsPlayer
+                    <div className="video" id="videoBox" style={{ marginTop: '50px' }}>
+                    {/* <div className="video" id="videoBox"> */}
+                        {/* <ReactHlsPlayer
                             url='https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8'
                             autoplay={false}
                             controls={true}
@@ -54,16 +51,16 @@ function Play(props) {
                             onPlay={() => adHandler('none')}
                             playerRef={videoEl}
                         />
-                        <img id="advertisePic" alt="ad" src={Advertise} className="ad" style={{ display: 'none' }}></img>
+                        <img id="advertisePic" alt="ad" src={Advertise} className="ad" style={{ display: 'none' }}></img> */}
                         {/* 以下Youtube版 */}
-                        {/* <ReactPlayer
+                        <ReactPlayer
                             url={urlVideo}
                             volume={0.8}
                             width='70%'
                             height={videoHeight}
                             controls={true}
                             onReady={props.setLoadingStatus}
-                        /> */}
+                        />
                     </div>
                     <div className="video" style={{ marginTop: '10px' }}>
                         <h3 className="play_title" style={{ width: '70%' }}>
